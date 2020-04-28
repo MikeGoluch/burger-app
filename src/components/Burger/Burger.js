e5e7c015fa81d93ce8d161bd2ad2a7c0c8d522be
MikeGoluch <goluch.michal89@gmail.com>
2020-04-28 20:30:25 +0200

    create ingredients component, test the component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+    let convertedIngredients = Object.keys(props.ingredients).map(ingredient => {
+        return [...Array(props.ingredients[ingredient])].map((amount, index) => {
+            return <BurgerIngredient key={ingredient + index} type={ingredient}/>
+        })
+    });
+    const reducedIngredientsArray = convertedIngredients.reduce((acc, cur) => {
+        return acc.concat(cur);
+    }, []);
+
+    if (reducedIngredientsArray.length === 0) {
+        convertedIngredients = <p>Start adding the ingredients</p>
+    }
+    console.log(reducedIngredientsArray)
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'/>
+            {convertedIngredients}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
+    )
+}
+
+export default burger
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
 
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            cheese: 0,
+            meat: 0,
+            salad: 0,
+            bacon: 0
+        }
+    }
+
     render() {
         return (
             <Auxillary>
                 <div>
-                    Burger
+                    <Burger ingredients={this.state.ingredients}/>
                 </div>
                 <div>
                     Build Controls
